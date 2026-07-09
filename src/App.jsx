6055aaae8e7d8c24538acf95eb70d2b37b38dd3f
Sourav Kumar <154079071+sourav8963@users.noreply.github.com>
2026-07-09 09:10:49 +0530

    Add NotFound route for unmatched paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Resume from './components/Resume'
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/resume' element={<Resume/>} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
     </BrowserRouter>
   )
